9d379a1822771719b92a5ce35d99851fe4d8bf3a
ahmedtechm <ahmedtechm2050@gmail.com>
2023-03-23 12:47:15 +0400

    Java program for connecting to a SQL Server database with user authentication

--- src/UniProjectAPI/MainClass.java
+++ src/UniProjectAPI/MainClass.java
@@ -1,11 +1,37 @@
 package UniProjectAPI;
 
+import java.sql.Connection;
 import java.util.Scanner;
+
 
 public class MainClass {
 
 	public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        
+		String url = "jdbc:sqlserver://localhost:1433;" + "databaseName=universityDB;" + "encrypt=true;"
+				+ "trustServerCertificate=true";
+
+		String userID = "sa";
+		String passID = "root";
+
+		System.out.println("Connecting to University database...");
+
+		boolean loggedIn = true;
+		while (loggedIn) {
+			System.out.println("\nEnter User ID: ");
+			userID = scanner.nextLine();
+			System.out.println("Enter User password: ");
+			passID = scanner.nextLine();
+
+			if (userID.equals("sa") && passID.equals("root")) {
+				loggedIn = false;
+			} else {
+				System.out.println("Wrong username or password");
+			}
+		}
+
+		Connection connection = null;
 
 	}
 
