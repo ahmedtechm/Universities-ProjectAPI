78a0bda08de22d57ac98ddf9021ce09ffe7866be
ahmedtechm <ahmedtechm2050@gmail.com>
2023-03-23 10:55:34 +0400

    Added Code for a class with getters and setters for a university's properties

--- src/UniProjectAPI/MyObject.java
+++ src/UniProjectAPI/MyObject.java
@@ -3,10 +3,49 @@ package UniProjectAPI;
 public class MyObject {
 
 	// Define your university object properties here
-	String state_province;
-	String domains[];
-	String country;
-	String web_pages[];
-	String name;
-	String alpha_two_code;
+	private  String state_province;
+	private  String domains[];
+	private  String country;
+	private  String web_pages[];
+	private  String name;
+	private  String alpha_two_code;
+	
+	
+	public String getState_province() {
+		return state_province;
+	}
+	public void setState_province(String state_province) {
+		this.state_province = state_province;
+	}
+	public String[] getDomains() {
+		return domains;
+	}
+	public void setDomains(String[] domains) {
+		this.domains = domains;
+	}
+	public String getCountry() {
+		return country;
+	}
+	public void setCountry(String country) {
+		this.country = country;
+	}
+	public String[] getWeb_pages() {
+		return web_pages;
+	}
+	public void setWeb_pages(String[] web_pages) {
+		this.web_pages = web_pages;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getAlpha_two_code() {
+		return alpha_two_code;
+	}
+	public void setAlpha_two_code(String alpha_two_code) {
+		this.alpha_two_code = alpha_two_code;
+	}
+	
 }
--- src/UniProjectAPI/UniversitiesAPI.java
+++ src/UniProjectAPI/UniversitiesAPI.java
@@ -10,18 +10,25 @@ import com.google.gson.Gson;
 public class UniversitiesAPI {
 
 	public static void main(String[] args) {
-
+		
+		// Define the API URL to retrieve a list of universities in Oman
 		String apiUrl = "http://universities.hipolabs.com/search?country=Oman";
+		
 		try {
+			// Create a URL object and establish a connection to the API
 			URL url = new URL(apiUrl);
 			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+			
+			// Set the HTTP request method and Accept header
 			conn.setRequestMethod("GET");
 			conn.setRequestProperty("Accept", "application/json");
-
+			
+			// Check if the HTTP response code indicates success (code 200)
 			if (conn.getResponseCode() != 200) {
 				throw new RuntimeException("HTTP error code : " + conn.getResponseCode());
 			}
-
+			
+			// Read the API response and store it as a JSON string
 			BufferedReader br = new BufferedReader(new InputStreamReader((conn.getInputStream())));
 			String output;
 			StringBuilder json = new StringBuilder();
@@ -31,11 +38,14 @@ public class UniversitiesAPI {
 			}
 
 			conn.disconnect();
-
+			
+			// Parse the JSON string into a Java object using Gson library
 			Gson gson = new Gson();
 			MyObject myObj = gson.fromJson(json.toString(), MyObject.class);
+			
+			// TODO: Use myObj for further processing (e.g. print university names, etc.)
 
-			// Use myObj for further processing
+
 
 		} catch (Exception e) {
 			e.printStackTrace();
