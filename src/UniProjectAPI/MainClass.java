48a2a1e68046c8a8c8a1f292db872eede5b270d8
ahmedtechm <ahmedtechm2050@gmail.com>
2023-03-23 13:24:40 +0400

    Implementing a menu with switch case

--- src/UniProjectAPI/MainClass.java
+++ src/UniProjectAPI/MainClass.java
@@ -51,8 +51,56 @@ public class MainClass {
 				System.out.print("\nEnter your choice: ");
 
 				int choice = scanner.nextInt();
+				
+				switch (choice) {
+                case 1:
+                    System.out.println("Initializing database...");
+                    
+                    break;
+                case 2:
+                    System.out.println("Fetching data from API...");
+                   
+                    break;
+                case 3:
+                    System.out.println("Fetching data from database...");
+                    
+                    break;
+                case 4:
+                    System.out.println("Printing universities...");
+                   
+                    break;
+                case 5:
+                    System.out.println("Searching universities By Country...");
+                    
+                    break;
+                case 6:
+                    System.out.println("Backing up database...");
+                    
+                    break;
+                case 7:
+                    System.out.println("Removing tables from database...");
+                    
+                    break;
+                case 8:
+                    System.out.println("Dumping data to file...");
+               
+                    break;
+                case 9:
+                    System.out.println("Retrieving data from file...");
+            
+                    break;
+                case 10:
+                    System.out.println("Exiting program...");
+                    running = false;
+                    break;
+                default:
+                    System.out.println("Invalid choice, please try again.");
+                    break;
+            }
+        }
 
+        scanner.close();
 
-	}}
+	}
 
 }
