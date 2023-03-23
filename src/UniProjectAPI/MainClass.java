4d44de96af5032de9f89a76dd5ac5aaba9bf49be
ahmedtechm <ahmedtechm2050@gmail.com>
2023-03-23 12:17:28 +0400

    Created Scanner object to read input from the user

---
+++ src/UniProjectAPI/MainClass.java
@@ -0,0 +1,12 @@
+package UniProjectAPI;
+
+import java.util.Scanner;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+	}
+
+}
